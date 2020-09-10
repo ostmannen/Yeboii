fc64ecbdee291bd33e67a765af758882b1d44e78
Niklas <niklas.ostman02@gmail.com>
2020-09-10 16:14:51 +0200

    Jag skapade projekt

---
+++ classer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace classer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            boll braboll = new boll() {name = "inteMax"};
+            System.Console.WriteLine(braboll.name);
+            System.Console.WriteLine(braboll.vikt);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ classer/boll.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace classer
+{
+    public class boll
+    {
+     public float pris = 10f;
+     public float vikt = 25.01f;
+     public string name;
+    }
+}
