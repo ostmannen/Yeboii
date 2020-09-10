d77e4693df6fc970e8bf247a2d89910555a80938
Niklas <niklas.ostman02@gmail.com>
2020-09-10 16:22:53 +0200

    ny boll

--- classer/Program.cs
+++ classer/Program.cs
@@ -7,6 +7,7 @@ namespace classer
         static void Main(string[] args)
         {
             boll braboll = new boll() {name = "inteMax, pris = 100.05f"};
+            boll vandligBoll = new boll() {name = "david", vikt = 10.001f};
             System.Console.WriteLine(braboll.name);
             System.Console.WriteLine(braboll.vikt);
             Console.ReadLine();
